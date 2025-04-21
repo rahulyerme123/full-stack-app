6e12b7fc1591b26a2bd29c47da4af86909f14d59
rahulyerme123 <106153203+rahulyerme123@users.noreply.github.com>
2025-04-21 15:25:36 +0530

    Update course.service.ts small case

--- frontend/src/app/services/course.service.ts
+++ frontend/src/app/services/course.service.ts
@@ -2,7 +2,7 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
-import { Course } from '../models/Course';
+import { Course } from '../models/course';
 // comment added
 @Injectable()
 export class CourseService {
